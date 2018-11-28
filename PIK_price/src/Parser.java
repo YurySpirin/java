eaa29caf6aac4112baa59dc591108b1711528b82
YurySpirin <45099821+YurySpirin@users.noreply.github.com>
2018-11-28 18:08:14 +0300

    1.3 last_stable

--- PIK_price/src/Parser.java
+++ PIK_price/src/Parser.java
@@ -1,13 +1,13 @@
 public class Parser{
 public static void main(String[] args) {
-    /*ParserEthalon p = new ParserEthalon();
+    ParserEthalon p = new ParserEthalon();
     System.out.println(p.getPrice());
     System.out.println(p.getFloor());
     System.out.println(p.getArea());
     ParserPik q = new ParserPik();
     System.out.println(q.getPrice());
     System.out.println(q.getFloor());
-    System.out.println(q.getArea());*/
+    System.out.println(q.getArea());
     }
   }
 
